05bc006f12211a7b4128bcb0f80b474f221d3116
segish <tsegatigneh08@example.com>
2023-11-11 03:04:02 +0300

    first commit

--- admin/controllers/Auth.js
+++ admin/controllers/Auth.js
@@ -81,6 +81,9 @@ const login = async (req, res) => {
         res.cookie("adminAccessToken", token, {
             expires: new Date(Date.now() + 1000 * 172800),  //to expire after 2 days
             httpOnly: true,
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
         }).status(200).json(others);
     } catch (err) {
         res.status(500).json("somthing went wrong!")
@@ -90,8 +93,9 @@ const login = async (req, res) => {
 //logout
 const logout = (req, res) => {
     res.clearCookie("adminAccessToken", {
-        // secure: true,
-        // sameSite: "none"
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
     }).status(200).json("user has been loged out")
 }
 
@@ -199,8 +203,9 @@ const changePwd = async (req, res) => {
             await currentUser.save()
 
             return res.clearCookie("adminAccessToken", {
-                // secure: true,
-                // sameSite: "none"
+                secure: true,
+                httpOnly: true,
+                sameSite: "strict",
             }).status(200).json("passwored change success")
 
         } catch (err) {
@@ -267,13 +272,18 @@ const getAdmin = async (req, res) => {
     if (!token) return res.status(401).json("You must login first!");
 
     jwt.verify(token, process.env.JWT_SECRETE_KEY, async (err, userInfo) => {
-        if (err) return res.status(403).json("Some thing went wrong please Logout and Login again ");
+        if (err) return res.clearCookie("adminAccessToken", {
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+        }) .status(403).json("Some thing went wrong please Logout and Login again ");
 
         try {
             const user = await Admin.findById(userInfo.id);
             if (!user) return res.clearCookie("adminAccessToken", {
-                // secure: true,
-                // sameSite: "none"
+                secure: true,
+                httpOnly: true,
+                sameSite: "strict",
             }).status(403).json("No account found!");
             const { password, updatedAt, ...others } = user._doc;
             res.status(200).json(others);
--- cashier/controllers/Auth.js
+++ cashier/controllers/Auth.js
@@ -21,6 +21,9 @@ const login = async (req, res) => {
         res.cookie("accessToken", token, {
             expires: new Date(Date.now() + 1000 * 172800),  //to expire after 2 days
             httpOnly: true,
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
         }).status(200).json(others);
     } catch (err) {
         res.status(500).json("somthing went wrong!")
@@ -30,8 +33,9 @@ const login = async (req, res) => {
 //logout
 const logout = (req, res) => {
     res.clearCookie("accessToken", {
-        // secure: true,
-        // sameSite: "none"
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
     }).status(200).json("user has been loged out")
 }//send otp
 
@@ -135,8 +139,9 @@ const changePwd = async (req, res) => {
             await currentUser.save()
 
             return res.clearCookie("accessToken", {
-                // secure: true,
-                // sameSite: "none"
+                secure: true,
+                httpOnly: true,
+                sameSite: "strict",
             }).status(200).json("passwored change success")
 
         } catch (err) {
@@ -149,17 +154,22 @@ const changePwd = async (req, res) => {
 //refresh user
 const getCashier = async (req, res) => {
     const token = req.cookies.accessToken;
-    if (!token) return res.clearCookie("accessToken", {
-        // secure: true,
-        // sameSite: "none"
-    }).status(401).json("You must login first!");
+    if (!token) return res.status(401).json("You must login first!");
 
     jwt.verify(token, process.env.JWT_SECRETE_KEY, async (err, userInfo) => {
-        if (err) return res.status(403).json("Some thing went wrong please Logout and Login again ");
+        if (err) return res.clearCookie("accessToken", {
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+        }).status(403).json("Some thing went wrong please Logout and Login again ");
 
         try {
             const user = await Cashier.findById(userInfo.id);
-            if (!user) return res.status(403).json("No account found!");
+            if (!user) return res.clearCookie("accessToken", {
+                secure: true,
+                httpOnly: true,
+                sameSite: "strict",
+            }).status(403).json("No account found!");
             const { password, updatedAt, ...others } = user._doc;
             res.status(200).json(others);
         } catch (err) {
